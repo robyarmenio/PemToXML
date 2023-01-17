724ea588852129a4e55f5397e6e03b825466b370
robyarmenio <45827756+robyarmenio@users.noreply.github.com>
2023-01-17 10:40:47 +0100

    Initial commit
    First working version

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+using System.Xml;
+
+Console.WriteLine("PEM to XML RSA Key Converter started...");
+
+string? filename = "";
+if (args.Length > 0)
+    filename = args[0];
+else
+{
+    Console.WriteLine("Please enter PEM filename:");
+    filename = Console.ReadLine();
+}
+
+if (!String.IsNullOrEmpty(filename))
+{
+    var privateKey = await File.ReadAllTextAsync(filename);
+    var rsa = RSA.Create();
+    rsa.ImportFromPem(privateKey.ToCharArray());
+    var d = new XmlDocument();
+    d.LoadXml(rsa.ToXmlString(true));
+
+    var xmlFilename = filename.EndsWith(".pem") ? filename.Replace(".pem", ".xml") : filename + ".xml";
+    d.Save(xmlFilename);
+}
